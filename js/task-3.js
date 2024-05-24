c4d622d4b507840b3c93f5e0e4e8bb0b98ee61e0
Vladimir Belogorodskiy <33459109+TyronNIKO@users.noreply.github.com>
2024-05-24 18:54:15 +0300

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,6 @@
+"use strict";
+
+const input = document.querySelector("#name-input");
+const nameOutput = document.querySelector("#name-output");
+const nameDefault = "Anonymous";
+input.addEventListener("input", e => (nameOutput.innerText = e.target.value.trim() || nameDefault));
